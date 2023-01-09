8f762dc8cfe5a4061c4cd4614be82282ea3fc8cd
SOURAV <devbhsourav@gmail.com>
2023-01-09 13:03:12 +0530

    [sourav]add : RepoLayer-UserEntity_FundooContext

---
+++ FundooNoteApp/RepositoryLayer/Context/FundooContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Context
+{
+    public class FundooContext : DbContext
+    {
+        public FundooContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<UserEntity> UserDetails { get; set; }
+    }
+}
---
+++ FundooNoteApp/RepositoryLayer/Entity/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
