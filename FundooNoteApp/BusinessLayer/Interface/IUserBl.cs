38989a911168b9a6db386019442f5c1a1f0a9c2d
SOURAV <devbhsourav@gmail.com>
2023-01-09 14:47:45 +0530

    [sourav]add : IUserBl_UserBl

---
+++ FundooNoteApp/BusinessLayer/Interface/IUserBl.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBl
+    {
+        public UserEntity RegisterUser(UserRegistration userRegistration);
+    }
+}
---
+++ FundooNoteApp/BusinessLayer/Service/UserBl.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class UserBl : IUserBl
+    {
+        IUserRl iUserRl;
+        public UserBl(IUserRl iUserRl)
+        {
+            this.iUserRl = iUserRl;
+        }
+
+        public UserEntity RegisterUser(UserRegistration userRegistration)
+        {
+            try
+            {
+                return iUserRl.RegisterUser(userRegistration);  
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
