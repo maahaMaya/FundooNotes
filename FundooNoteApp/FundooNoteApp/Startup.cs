35bedeb37cf89e03ae4906a20452b408b66c5db3
SOURAV <devbhsourav@gmail.com>
2023-01-11 19:20:18 +0530

    [sourav]add : swagger

--- FundooNoteApp/FundooNoteApp/Startup.cs
+++ FundooNoteApp/FundooNoteApp/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using RepositoryLayer.Context;
 using RepositoryLayer.Interface;
 using RepositoryLayer.Service;
@@ -35,6 +36,17 @@ namespace FundooNoteApp
             services.AddControllers();
             services.AddTransient<IUserBl, UserBl>();
             services.AddTransient<IUserRl, UserRl>();
+
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Implement Swagger UI",
+                    Description = "A simple example to Implement Swagger UI",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +66,11 @@ namespace FundooNoteApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Showing API V1");
             });
         }
     }
